4ec9fe9454ccf44fa15c818dc5878f040f38048a
Panos  Patros <patrospanos@gmail.com>
2023-09-27 09:01:28 +1300

    Captured all (commented out some) windows lifecycle events

--- Raygun4Maui/MauiRum/ApplicationLifecycle/RaygunRumWindowExtensions.cs
+++ Raygun4Maui/MauiRum/ApplicationLifecycle/RaygunRumWindowExtensions.cs
@@ -10,10 +10,43 @@ namespace Raygun4Maui.MauiRum.ApplicationLifecycle
 #if WINDOWS
             builder.AddWindows(windows =>
             {
+                windows.OnActivated((app, args) => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Activated!", new Dictionary<String, String> { { "app", app.ToString() }, { "args", args.ToString() } });
+                });
+
+                windows.OnClosed((app, args) => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Closed!", new Dictionary<String, String> { { "app", app.ToString() }, { "args", args.ToString() } });
+                });
+
                 windows.OnLaunched((app, args) =>
                 {
-                    raygunRumEventTracker.TrackEvent("Windows application launched!");
+                    raygunRumEventTracker.TrackEvent("Windows Application Launched!", new Dictionary<String, String> { { "app", app.ToString() }, { "args", args.ToString() } });
                 });
+
+                windows.OnLaunching((app, args) => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Launching!", new Dictionary<String, String> { { "app", app.ToString() }, { "args", args.ToString() } });
+                });
+
+                /*windows.OnPlatformMessage((app, args) => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Platform Message!", new Dictionary<String, String> { { "app", app.ToString() }, { "args", args.ToString() } });
+                });*/
+
+                /*windows.OnPlatformWindowSubclassed((app, args) => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Platform Window Subclassed!", new Dictionary<String, String> { { "app", app.ToString() }, { "args", args.ToString() } });
+                });*/
+
+                windows.OnResumed(window => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Resumed!", new Dictionary<String, String> { { "window", window.ToString() }});
+                });
+
+                windows.OnVisibilityChanged((app, args) => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Visibility Changed!", new Dictionary<String, String> { { "app", app.ToString() }, { "args", args.ToString() } });
+                });
+
+                windows.OnWindowCreated(window => {
+                    raygunRumEventTracker.TrackEvent("Windows Application Window Created!", new Dictionary<String, String> { { "window", window.ToString() } });
+                });
+
             });
 #endif
 
