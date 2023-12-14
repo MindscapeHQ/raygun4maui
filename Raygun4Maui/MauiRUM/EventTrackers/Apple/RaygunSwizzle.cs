89c7b404e664ca022bdd838943cc9a0adc506914
ProRedCat <43058406+ProRedCat@users.noreply.github.com>
2023-12-14 13:41:25 +1300

    iOS and MacCatalyst View Observer Swizzling
    Requires testing

---
+++ Raygun4Maui/MauiRUM/EventTrackers/Apple/RaygunSwizzle.cs
@@ -0,0 +1,71 @@
+﻿#if IOS || MACCATALYST
+using System.Runtime.InteropServices;
+using Foundation;
+using ObjCRuntime;
+#endif
+
+namespace Raygun4Maui.MauiRUM.EventTrackers.Apple;
+
+public class RaygunSwizzle
+{
+#if IOS || MACCATALYST
+    [DllImport("/usr/lib/libobjc.dylib")]
+    public extern static IntPtr class_getInstanceMethod(IntPtr classHandle, IntPtr Selector);
+
+    [DllImport("/usr/lib/libobjc.dylib")]
+    public extern static IntPtr imp_implementationWithBlock(ref BlockLiteral block);
+
+    [DllImport("/usr/lib/libobjc.dylib")]
+    public extern static void method_setImplementation(IntPtr method, IntPtr imp);
+
+    [DllImport("/usr/lib/libobjc.dylib")]
+    public extern static IntPtr method_getImplementation(IntPtr method);
+
+    public delegate void CaptureDelegate(IntPtr block, IntPtr self);
+
+    public delegate void CaptureBooleanDelegate(IntPtr block, IntPtr self, bool b);
+    
+    [MonoNativeFunctionWrapper]
+    public delegate void OriginalBooleanDelegate(IntPtr self, bool b);
+
+    public static void Hijack(NSObject obj, string selector, ref IntPtr originalImpl, CaptureDelegate captureDelegate)
+    {
+        var method = class_getInstanceMethod(obj.ClassHandle, new Selector(selector).Handle);
+        originalImpl = method_getImplementation(method);
+
+        if (originalImpl != IntPtr.Zero)
+        {
+            var block_value = new BlockLiteral();
+            block_value.SetupBlock(captureDelegate, null);
+
+            var imp = imp_implementationWithBlock(ref block_value);
+            method_setImplementation(method, imp);
+        }
+    }
+
+    public static void Hijack(NSObject obj, string selector, ref IntPtr originalImpl,
+        CaptureBooleanDelegate captureDelegate)
+    {
+        var method = class_getInstanceMethod(obj.ClassHandle, new Selector(selector).Handle);
+        originalImpl = method_getImplementation(method);
+
+        if (originalImpl != IntPtr.Zero)
+        {
+            var block_value = new BlockLiteral();
+            block_value.SetupBlock(captureDelegate, null);
+
+            var imp = imp_implementationWithBlock(ref block_value);
+            method_setImplementation(method, imp);
+        }
+    }
+
+    public static void Restore(NSObject obj, string selector, IntPtr originalImpl)
+    {
+        if (originalImpl != IntPtr.Zero)
+        {
+            var method = class_getInstanceMethod(obj.ClassHandle, new Selector(selector).Handle);
+            method_setImplementation(method, originalImpl);
+        }
+    }
+#endif
+}
---
+++ Raygun4Maui/MauiRUM/EventTrackers/Apple/RaygunUiViewControllerObserver.cs
@@ -0,0 +1,145 @@
+﻿#if IOS || MACCATALYST
+using System.Runtime.InteropServices;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+using Raygun4Maui.AppEvents;
+#endif
+
+
+namespace Raygun4Maui.MauiRUM.EventTrackers.Apple;
+
+public class RaygunUiViewControllerObserver
+{
+#if IOS || MACCATALYST
+    public RaygunUiViewControllerObserver()
+    {
+    }
+
+    private static IntPtr _originalLoadViewImp;
+    private static IntPtr _originalViewDidLoadImp;
+    private static IntPtr _originalViewWillAppearImp;
+    private static IntPtr _originalViewDidAppearImp;
+
+    public delegate void CaptureDelegate(IntPtr block, IntPtr self);
+
+    public delegate void CaptureBooleanDelegate(IntPtr block, IntPtr self, bool b);
+
+    public delegate void OriginalDelegate(IntPtr self);
+
+    [MonoNativeFunctionWrapper]
+    public delegate void OriginalBooleanDelegate(IntPtr self, bool b);
+
+    public void Register()
+    {
+        RaygunSwizzle.Hijack(new UIViewController(), "loadView", ref _originalLoadViewImp, OnLoadViewCapture);
+        RaygunSwizzle.Hijack(new UIViewController(), "viewDidLoad", ref _originalViewDidLoadImp, OnViewDidLoadCapture);
+        RaygunSwizzle.Hijack(new UIViewController(), "viewWillAppear:", ref _originalViewWillAppearImp,
+            OnViewWillAppearCapture);
+        RaygunSwizzle.Hijack(new UIViewController(), "viewDidAppear:", ref _originalViewDidAppearImp,
+            OnViewDidAppearCapture);
+    }
+
+    public void DeRegister()
+    {
+        RaygunSwizzle.Restore(new UIViewController(), "loadView", _originalLoadViewImp);
+        RaygunSwizzle.Restore(new UIViewController(), "viewDidLoad", _originalViewDidLoadImp);
+        RaygunSwizzle.Restore(new UIViewController(), "viewWillAppear:", _originalViewWillAppearImp);
+        RaygunSwizzle.Restore(new UIViewController(), "viewDidAppear:", _originalViewDidAppearImp);
+    }
+
+    //
+    // Load View
+    //
+
+    [MonoPInvokeCallback(typeof(CaptureDelegate))]
+    private static void OnLoadViewCapture(IntPtr block, IntPtr self)
+    {
+        string name = GetName(Runtime.GetNSObject(self));
+
+        RaygunAppEventPublisher.Publish(new ViewTimingStarted()
+        {
+            Name = name,
+            OccurredOn = DateTime.UtcNow.Ticks
+        });
+
+        var orig = (OriginalDelegate)Marshal.GetDelegateForFunctionPointer(_originalLoadViewImp,
+            typeof(OriginalDelegate));
+        orig(self);
+    }
+
+    //
+    // View Did Load
+    //
+
+    [MonoPInvokeCallback(typeof(CaptureDelegate))]
+    private static void OnViewDidLoadCapture(IntPtr block, IntPtr self)
+    {
+        string name = GetName(Runtime.GetNSObject(self));
+
+        RaygunAppEventPublisher.Publish(new ViewTimingStarted()
+        {
+            Name = name,
+            OccurredOn = DateTime.UtcNow.Ticks
+        });
+
+        var orig = (OriginalDelegate)Marshal.GetDelegateForFunctionPointer(_originalViewDidLoadImp,
+            typeof(OriginalDelegate));
+        orig(self);
+    }
+
+    //
+    // View Will Appear
+    //
+
+    [MonoPInvokeCallback(typeof(CaptureBooleanDelegate))]
+    private static void OnViewWillAppearCapture(IntPtr block, IntPtr self, bool animated)
+    {
+        string name = GetName(Runtime.GetNSObject(self));
+
+        RaygunAppEventPublisher.Publish(new ViewTimingStarted()
+        {
+            Name = name,
+            OccurredOn = DateTime.UtcNow.Ticks
+        });
+
+        var orig = (OriginalBooleanDelegate)Marshal.GetDelegateForFunctionPointer(_originalViewWillAppearImp,
+            typeof(OriginalBooleanDelegate));
+        orig(self, animated);
+    }
+
+    //
+    // View Did Appear
+    //
+
+    [MonoPInvokeCallback(typeof(CaptureBooleanDelegate))]
+    private static void OnViewDidAppearCapture(IntPtr block, IntPtr self, bool animated)
+    {
+        var orig = (OriginalBooleanDelegate)Marshal.GetDelegateForFunctionPointer(_originalViewDidAppearImp,
+            typeof(OriginalBooleanDelegate));
+        orig(self, animated);
+
+        string name = GetName(Runtime.GetNSObject(self));
+
+        RaygunAppEventPublisher.Publish(new ViewTimingFinished()
+        {
+            Name = name,
+            OccurredOn = DateTime.UtcNow.Ticks
+        });
+    }
+
+    private static string GetName(NSObject obj)
+    {
+        var objName = obj.ToString();
+        string name = objName.Replace("<", string.Empty).Replace(">", string.Empty).Replace("_", ".");
+
+        var index = name.IndexOf(':');
+        if (index >= 0 && index < name.Length)
+        {
+            name = name.Substring(0, index);
+        }
+
+        return name;
+    }
+#endif
+}
