13351aa495c3e7247c81f948428631f4bdb407fa
ProRedCat <43058406+ProRedCat@users.noreply.github.com>
2023-12-21 10:56:39 +1300

    Minimal fix
    Lock the environment variable assignment as this causes the Telerik issue (and issues with mass spamming). Also included change for Android unhandled exception to set the Handled to be true.

--- Raygun4Maui/RaygunMauiClient.cs
+++ Raygun4Maui/RaygunMauiClient.cs
@@ -1,4 +1,3 @@
-
 using System.Reflection;
 using Mindscape.Raygun4Net;
 using System.Globalization;
@@ -11,9 +10,16 @@ namespace Raygun4Maui
         private static RaygunMauiClient _instance;
         public static RaygunMauiClient Current => _instance;
 
+        private static readonly object EnvironmentLock = new();
+
         private static readonly string Name = Assembly.GetExecutingAssembly().GetName().Name;
-        private static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-        private static readonly string ClientUrl = "https://github.com/MindscapeHQ/raygun4maui"; //It does not seem like this can be obtained automatically
+
+        private static readonly string Version =
+            Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown";
+
+        private static readonly string
+            ClientUrl =
+                "https://github.com/MindscapeHQ/raygun4maui"; //It does not seem like this can be obtained automatically
 
         public static readonly RaygunClientMessage clientMessage = new()
         {
@@ -21,46 +27,55 @@ namespace Raygun4Maui
             Version = Version,
             ClientUrl = ClientUrl
         };
+
         internal static void Attach(RaygunMauiClient client)
         {
             if (_instance != null)
             {
                 throw new Exception("You should only call 'AddRaygun4maui' once in your app.");
             }
-          
+
             _instance = client;
         }
+
         public RaygunMauiClient(string apiKey) : base(apiKey)
         {
-
         }
 
         public RaygunMauiClient(RaygunSettings settings) : base(settings)
         {
+            System.Diagnostics.Debug.WriteLine("RAYGUN4MAUI CREATED");
         }
 
-        
-
-        protected override async Task<RaygunMessage> BuildMessage(Exception exception, IList<string> tags, IDictionary userCustomData, RaygunIdentifierMessage userInfo)
+        protected override async Task<RaygunMessage> BuildMessage(Exception exception, IList<string> tags,
+            IDictionary userCustomData, RaygunIdentifierMessage userInfo)
         {
-            DateTime now = DateTime.Now;
-            var environment = new RaygunMauiEnvironmentMessage //Mostlikely should be static
+            RaygunMauiEnvironmentMessage environment;
+            lock (EnvironmentLock)
             {
-                UtcOffset = TimeZoneInfo.Local.GetUtcOffset(now).TotalHours,
-                Locale = CultureInfo.CurrentCulture.DisplayName,
-                OSVersion = DeviceInfo.Current.VersionString,
-                Architecture = NativeDeviceInfo.Architecture(),
-                WindowBoundsWidth = DeviceDisplay.MainDisplayInfo.Width,
-                WindowBoundsHeight = DeviceDisplay.MainDisplayInfo.Height,
-                DeviceManufacturer = DeviceInfo.Current.Manufacturer,
-                Platform = NativeDeviceInfo.Platform(),
-                Model = DeviceInfo.Current.Model,
-                ProcessorCount = Environment.ProcessorCount,
-                ResolutionScale = DeviceDisplay.MainDisplayInfo.Density,
-                TotalPhysicalMemory = NativeDeviceInfo.TotalPhysicalMemory(),
-                AvailablePhysicalMemory = NativeDeviceInfo.AvailablePhysicalMemory(),
-                CurrentOrientation = DeviceDisplay.MainDisplayInfo.Orientation.ToString(),
-            };
+                DateTime now = DateTime.Now;
+
+                environment = new RaygunMauiEnvironmentMessage //Mostlikely should be static
+                {
+                    // Combination of these cause the error, it may happen with the other ones too
+                    UtcOffset = TimeZoneInfo.Local.GetUtcOffset(now).TotalHours, // Not this,
+                    Locale = CultureInfo.CurrentCulture.DisplayName, // Not this,
+                    OSVersion = DeviceInfo.Current.VersionString, // Not This,
+                    Architecture = NativeDeviceInfo.Architecture(), // Not This,
+                    WindowBoundsWidth = DeviceDisplay.MainDisplayInfo.Width, // <------ THIS!!
+                    
+                    
+                    WindowBoundsHeight = DeviceDisplay.MainDisplayInfo.Height,
+                    DeviceManufacturer = DeviceInfo.Current.Manufacturer,
+                    Platform = NativeDeviceInfo.Platform(),
+                    Model = DeviceInfo.Current.Model,
+                    ProcessorCount = Environment.ProcessorCount, 
+                    ResolutionScale = DeviceDisplay.MainDisplayInfo.Density,
+                    TotalPhysicalMemory = NativeDeviceInfo.TotalPhysicalMemory(),
+                    AvailablePhysicalMemory = NativeDeviceInfo.AvailablePhysicalMemory(),
+                    CurrentOrientation = DeviceDisplay.MainDisplayInfo.Orientation.ToString(),
+                };
+            }
 
             var details = new RaygunMessageDetails
             {
@@ -70,7 +85,8 @@ namespace Raygun4Maui
                 UserCustomData = userCustomData,
                 Tags = tags,
                 Version = ApplicationVersion,
-                User = userInfo ?? UserInfo ?? (!String.IsNullOrEmpty(User) ? new RaygunIdentifierMessage(User) : null),
+                User = userInfo ??
+                       UserInfo ?? (!String.IsNullOrEmpty(User) ? new RaygunIdentifierMessage(User) : null),
                 Environment = environment
             };
 
@@ -90,4 +106,4 @@ namespace Raygun4Maui
             return message;
         }
     }
-} 
+}
