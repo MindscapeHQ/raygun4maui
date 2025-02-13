ddd689625a7324298551ca8cd63fff15c822ef8b
sumitramanga <36393794+sumitramanga@users.noreply.github.com>
2025-02-13 16:35:37 +1300

    Update NetworkMonitor library

---
+++ Raygun4Maui.Binding.NetworkMonitor.iOS/Frameworks/RaygunNetworkMonitor.xcframework/ios-arm64-maccatalyst/RaygunNetworkMonitor.framework/Versions/A/Headers/NetworkMonitor_MacCatalyst.h
@@ -0,0 +1,18 @@
+//
+//  NetworkMonitor_MacCatalyst.h
+//  NetworkMonitor MacCatalyst
+//
+//  Created by dev on 15/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkMonitor_MacCatalyst.
+FOUNDATION_EXPORT double NetworkMonitor_MacCatalystVersionNumber;
+
+//! Project version string for NetworkMonitor_MacCatalyst.
+FOUNDATION_EXPORT const unsigned char NetworkMonitor_MacCatalystVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkMonitor_MacCatalyst/PublicHeader.h>
+
+
